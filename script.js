82efed16c47a1687c5b49fbbe26acb249cb10917
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-14 15:55:08 -0300

    element selection / form submit and input

---
+++ script.js
@@ -0,0 +1,18 @@
+// Element selection
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+// Functions
+
+// Events
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = todoInput.value;
+  if (inputValue) {
+    console.log(inputValue);
+  }
+});
